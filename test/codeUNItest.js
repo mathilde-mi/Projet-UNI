337db52f72bfcbce36c3527685fbaf680cb85809
mathilde-mi <122474582+mathilde-mi@users.noreply.github.com>
2023-02-14 11:17:37 +0100

    Ajout de loadFixture

--- test/codeUNItest.js
+++ test/codeUNItest.js
@@ -1,13 +1,19 @@
 const { expect } = require("chai");
 const { ethers } = require('hardhat');
+const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
 
 describe("Uni", function () {
+  async function deployUniFixture() {
+     const [owner, addr1, addr2] = await ethers.getSigners();
+     const mintingAllowedAfter_ = Date.now() * 24;
+     const Uni = await ethers.getContractFactory("Uni");
+     const hardhatUni = await Uni.deploy(owner.address, addr1.address, mintingAllowedAfter_);
+
+     return { Uni, hardhatUni, mintingAllowedAfter_, owner, addr1, addr2 };
+     };
 
   it("Deployment should assign the total supply of tokens to the owner", async function () {
-    const [owner, addr1, addr2] = await ethers.getSigners();
-    const mintingAllowedAfter_ = Date.now() * 24;
-    const Uni = await ethers.getContractFactory("Uni");
-    const hardhatUni = await Uni.deploy(owner.address, addr1.address, mintingAllowedAfter_);
+    const { hardhatUni, owner } = await loadFixture(deployUniFixture);
 
     const ownerBalance = await hardhatUni.balanceOf(owner.address);
     expect(await hardhatUni.totalSupply()).to.equal(ownerBalance);
